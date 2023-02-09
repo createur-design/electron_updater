6f039aa1c49864dd5e0b1ec27b7bbc54593f8f3e
Christophe Delobel <cdelobel.ext@simplon.co>
2023-02-09 14:49:22 +0100

    v1.0.8

--- preload.js
+++ preload.js
@@ -7,25 +7,25 @@ contextBridge.exposeInMainWorld("versions", {
   ping: () => ipcRenderer.invoke("ping"),
   // we can also expose variables, not just functions
 });
+window.addEventListener("DOMContentLoaded", async () => {
+  const notification = document.getElementById("notification");
+  const message = document.getElementById("message");
+  const restartButton = document.getElementById("restart-button");
 
-const notification = document.getElementById("notification");
-const message = document.getElementById("message");
-const restartButton = document.getElementById("restart-button");
+  ipcRenderer.on("update_available", () => {
+    ipcRenderer.removeAllListeners("update_available");
+    message.innerText = "A new update is available. Downloading now...";
+    notification.classList.remove("hidden");
+  });
 
-ipcRenderer.on("update_available", () => {
-  ipcRenderer.removeAllListeners("update_available");
-  message.innerText = "A new update is available. Downloading now...";
-  notification.classList.remove("hidden");
+  ipcRenderer.on("update_downloaded", () => {
+    ipcRenderer.removeAllListeners("update_downloaded");
+    message.innerText =
+      "Update Downloaded. It will be installed on restart. Restart now?";
+    restartButton.classList.remove("hidden");
+    notification.classList.remove("hidden");
+  });
 });
-
-ipcRenderer.on("update_downloaded", () => {
-  ipcRenderer.removeAllListeners("update_downloaded");
-  message.innerText =
-    "Update Downloaded. It will be installed on restart. Restart now?";
-  restartButton.classList.remove("hidden");
-  notification.classList.remove("hidden");
-});
-
 contextBridge.exposeInMainWorld("api", {
   closeNotification: () => {
     notification.classList.add("hidden");
