9ccb37ef1cdc33cbc9d729d21697f24cb0f3fc51
Christophe Delobel <cdelobel.ext@simplon.co>
2023-02-09 15:03:25 +0100

    v1.0.10

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require("electron");
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
 const { autoUpdater } = require("electron-updater");
 const path = require("path");
 
